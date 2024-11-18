9531b95a7b216e5d9f0d0e19ba310002b1035ca5
Kale Williams <klaeboy.williams@gmail.com>
2024-11-17 19:43:32 -0500

    Changed heroes list call to ajax method

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 $(document).ready(function(){
     $(".search").click(function() {
-        const httpRequest = new XMLHttpRequest();
-        let url = "http://localhost/info2180-lab4/superheroes.php";
-        httpRequest.onreadystatechange = alertHeroes;
-        httpRequest.open('GET', url);
-        httpRequest.send();
-
-        function alertHeroes() {
-            if (httpRequest.readyState === XMLHttpRequest.DONE) {
-             if (httpRequest.status === 200) {
-             let response = httpRequest.responseText;
-             alert(response);
-             } else {
-             alert('There was a problem with the request.');
-             }
-            }
-        }
+        $.ajax("http://localhost/info2180-lab4/superheroes.php")
+        .done(function(result) {
+        alert(result);
+        }).fail(function(result) {
+        alert("There seems to be an error.");
+        });
     }); 
 
     
